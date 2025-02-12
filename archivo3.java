25de4641fa78f6f187a7809e90b1aadf839b5f4f
Aitor <ikamindeguia24@lhusurbil.eus>
2025-02-12 08:26:04 +0100

    subiendo archivos a repositorio remoto

---
+++ archivo3.java
@@ -0,0 +1,5 @@
+public class archivo3{
+        public static void main(String[] args){
+                System.out println("Me llamo Aitor");
+        }
+}
---
+++ password.py
@@ -0,0 +1,1 @@
+print("No se programar en python");
